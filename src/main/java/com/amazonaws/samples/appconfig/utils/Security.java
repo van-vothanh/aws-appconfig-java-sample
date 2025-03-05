23eb2f53fb2d87559860047fc840553b580561ca
Java-Version-Upgrade <n/a>
2025-03-05 04:14:34 +0000

    Automated commit for Q Code Transformation Job: 91b3cbf3-54da-4bf0-a610-e10799a5370c

--- src/main/java/com/amazonaws/samples/appconfig/utils/Security.java
+++ src/main/java/com/amazonaws/samples/appconfig/utils/Security.java
@@ -1,23 +1,20 @@
 //javax.security.cert to java.security.cert classes migration
 package com.amazonaws.samples.appconfig.utils;
 
-import javax.security.cert.*;
+import java.security.cert.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 
 public class Security {
-    public Certificate getCertificate(File certFile) throws CertificateExpiredException, CertificateNotYetValidException {
+    public Certificate getCertificate(File certFile) throws CertificateException, FileNotFoundException, IOException {
         X509Certificate cert = null;
-        try {
-            InputStream inStream = new FileInputStream(certFile);
-            cert = X509Certificate.getInstance(inStream);
-        } catch (CertificateException e) {
-            throw new RuntimeException(e);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        try (InputStream inStream = new FileInputStream(certFile)) {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            cert = (X509Certificate) cf.generateCertificate(inStream);
         }
         cert.checkValidity(new Date());
         return cert;

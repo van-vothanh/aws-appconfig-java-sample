66bde1e3996ecd0e050ca75cecc9489a12504406
Version Upgrade <n/a>
2025-12-03 02:54:26 +0000

    Upgrade to Java 21 on Iteration 2

--- src/test/java/com/amazonaws/samples/appconfig/movies/MockTest.java
+++ src/test/java/com/amazonaws/samples/appconfig/movies/MockTest.java
@@ -1,15 +1,15 @@
 package com.amazonaws.samples.appconfig.movies;
-import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyInt;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 
 
@@ -29,6 +29,7 @@ public class MockTest {
 
     @Test
     public void creatingASpyOnArrayList() {
+        @SuppressWarnings("unchecked")
         List<String> listSpy = spy(ArrayList.class);
         listSpy.add("Paid");
         listSpy.add("Movies");
@@ -40,7 +41,7 @@ public class MockTest {
 
     @Test
     public void letsMockListSizeWithMultipleReturnValues() {
-        List list = mock(List.class);
+        List<String> list = mock(List.class);
         Mockito.when(list.size()).thenReturn(10).thenReturn(20);
         assertEquals(10, list.size()); // First Call
         assertEquals(20, list.size()); // Second Call

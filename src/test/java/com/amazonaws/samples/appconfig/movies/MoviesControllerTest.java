ca54b37b7cf3fb4968e3463862961b1ad6335c66
Version Upgrade <n/a>
2026-01-05 20:33:52 +0000

    Upgrade to Java 21 on Iteration 2

--- src/test/java/com/amazonaws/samples/appconfig/movies/MoviesControllerTest.java
+++ src/test/java/com/amazonaws/samples/appconfig/movies/MoviesControllerTest.java
@@ -3,10 +3,11 @@ package com.amazonaws.samples.appconfig.movies;
 import com.amazonaws.samples.appconfig.utils.AppConfigUtility;
 import com.amazonaws.samples.appconfig.cache.ConfigurationCache;
 import com.amazonaws.samples.appconfig.model.ConfigurationKey;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.env.Environment;
 import software.amazon.awssdk.services.appconfig.AppConfigClient;
 import software.amazon.awssdk.services.appconfig.model.GetConfigurationResponse;
@@ -14,10 +15,12 @@ import software.amazon.awssdk.services.appconfig.model.GetConfigurationResponse;
 import java.time.Duration;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
+@ExtendWith(MockitoExtension.class)
 public class MoviesControllerTest {
 
     @Mock
@@ -31,42 +34,19 @@ public class MoviesControllerTest {
 
     private MoviesController moviesController;
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
         moviesController = new MoviesController();
         moviesController.env = env;
     }
 
     @Test
     public void testMovieWithFeatureEnabled() {
-        // Arrange
-        when(env.getProperty("appconfig.application")).thenReturn("myApp");
-        when(env.getProperty("appconfig.environment")).thenReturn("dev");
-        when(env.getProperty("appconfig.config")).thenReturn("myConfig");
-        when(env.getProperty("appconfig.cacheTtlInSeconds")).thenReturn("60");
-
-        String jsonResponse = "{\"boolEnableFeature\":true,\"intItemLimit\":5}";
-
-        GetConfigurationResponse getConfigurationResponse = GetConfigurationResponse.builder().build();
-
-        AppConfigUtility appConfigUtility = mock(AppConfigUtility.class);
-        when(appConfigUtility.getConfiguration(any(ConfigurationKey.class))).thenReturn(getConfigurationResponse);
-
-        moviesController.cacheItemTtl = Duration.ofSeconds(60);
-        moviesController.client = appConfigClient;
-        moviesController.cache = configurationCache;
-        moviesController.clientId = UUID.randomUUID().toString();
-
-        // Act
-        //Movie[] movies = moviesController.movie();
-
         // Assert
         Movie[] expectedMovies = new Movie[5];
         for (int i = 0; i < 5; i++) {
             expectedMovies[i] = MoviesController.PAIDMOVIES[i];
         }
-        //assertArrayEquals(expectedMovies, movies);
         assertEquals(5, expectedMovies.length);
     }
 

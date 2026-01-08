bd83488829c98e3e6f5d3a6dc3b799acb292912c
Version Upgrade <n/a>
2026-01-08 05:55:05 +0000

    Step 9: Update Testing Framework Dependencies Build status: Success

--- src/test/java/com/amazonaws/samples/appconfig/movies/MathTest.java
+++ src/test/java/com/amazonaws/samples/appconfig/movies/MathTest.java
@@ -1,11 +1,11 @@
 package com.amazonaws.samples.appconfig.movies;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import com.amazonaws.samples.appconfig.utils.Math;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathTest {
 
